b4ec3274f690d8e67120e2444aa172fc396f3f25
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-04-22 15:42:33 +0900

    Change due to module interface change

--- face_tracking_picamera.py
+++ face_tracking_picamera.py
@@ -4,11 +4,14 @@
 Face Tracking PiCamera
 """
 
-import sys
-sys.path.append('raspberrypi_lib')
 import time
+
 import cv2
-from camera_mount import CameraMount
+
+from raspberrypi_lib.picamera_v2 import PiCameraV2
+from raspberrypi_lib.sg90 import SG90, SG90HW
+from raspberrypi_lib.camera_mount import CameraMount
+
 
 STREAMING_WIDTH = 480
 STREAMING_HEIGHT = 480
@@ -89,80 +92,81 @@ def pixcel2angle(x, y):
 
 
 if __name__ == '__main__':
-    with CameraMount() as camera_mount:
-        # initial camera position
-        camera_x_angle, camera_y_angle = 0, 0
-        camera_mount.position(camera_x_angle, camera_y_angle)
-        time.sleep(0.1)
+    with PiCameraV2() as c, SG90HW(18) as sp, SG90HW(19) as st:
+        with CameraMount(c, sp, st) as camera_mount:
+            # initial camera position
+            camera_x_angle, camera_y_angle = 0, 0
+            camera_mount.position(camera_x_angle, camera_y_angle)
+            time.sleep(0.1)
 
-        # start camera streaming
-        camera = camera_mount.camera
-        camera.start_streaming(STREAMING_WIDTH, STREAMING_HEIGHT)
+            # start camera streaming
+            camera = camera_mount.camera
+            camera.start_streaming(STREAMING_WIDTH, STREAMING_HEIGHT)
 
-        try:
-            if SHOW_IMAGE:
-                cv2.namedWindow(WINDOW_TITLE, cv2.WINDOW_NORMAL)
-        
-            if WRITE_VIDEO:
-                fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                video = cv2.VideoWriter(OUTPUT_VIDEO_FILE, fourcc, VIDEO_FRAMERATE, (STREAMING_WIDTH, STREAMING_HEIGHT))
-
-            while True:
-                frame = camera.frame
-                face_list = get_face_position_with_eye(frame)
-
-                # draw rect of all face area
-                for (x, y, w, h) in face_list:
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), FACE_RECT_COLOR, RECT_THICKNESS)
-
-                # tracking target face
-                if len(face_list):
-                    # re-draw rect of target face area
-                    (x, y, w, h) = get_largest_image(face_list)
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), TARGET_RECT_COLOR, RECT_THICKNESS)
-
-                    # calculate the amount of camera-movement to center the target
-                    delta_x_angle, delta_y_angle = pixcel2angle(x + (w // 2), y + (h // 2))
-
-                    camera_x_angle += delta_x_angle * CORRECTION_FUCTOR
-                    camera_y_angle += delta_y_angle * CORRECTION_FUCTOR
-
-                    # limitation of upper and lower
-                    if camera_x_angle < MIN_X_CAMERA_ANGLE:
-                        camera_x_angle = MIN_X_CAMERA_ANGLE
-                    elif camera_x_angle > MAX_X_CAMERA_ANGLE:
-                        camera_x_angle = MAX_X_CAMERA_ANGLE
-
-                    if camera_y_angle < MIN_Y_CAMERA_ANGLE:
-                        camera_y_angle = MIN_Y_CAMERA_ANGLE
-                    elif camera_y_angle > MAX_Y_CAMERA_ANGLE:
-                        camera_y_angle = MAX_Y_CAMERA_ANGLE
-
-                    # change camera position
-                    camera_mount.position(camera_x_angle, camera_y_angle)
-
-                    print("delta", ("{:.2f}".format(delta_x_angle), "{:.2f}".format(delta_y_angle)))
-                    print("camera", ("{:.2f}".format(camera_x_angle), "{:.2f}".format(camera_y_angle)))
-                    print()
-
-                # show image
+            try:
                 if SHOW_IMAGE:
-                    cv2.imshow(WINDOW_TITLE, frame)
-
-                # write video
+                    cv2.namedWindow(WINDOW_TITLE, cv2.WINDOW_NORMAL)
+            
                 if WRITE_VIDEO:
-                    video.write(frame)
+                    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                    video = cv2.VideoWriter(OUTPUT_VIDEO_FILE, fourcc, VIDEO_FRAMERATE, (STREAMING_WIDTH, STREAMING_HEIGHT))
 
-                # key wait for escape
-                if cv2.waitKey(KEY_WAIT_TIME) & 0xFF == ESC_KEY_NUM:
-                    break
+                while True:
+                    frame = camera.frame
+                    face_list = get_face_position_with_eye(frame)
 
-        except KeyboardInterrupt:
-            print('quit face tracking')
-        
-        finally:
-            if SHOW_IMAGE:
-                cv2.destroyAllWindows()
+                    # draw rect of all face area
+                    for (x, y, w, h) in face_list:
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), FACE_RECT_COLOR, RECT_THICKNESS)
 
-            if WRITE_VIDEO:
-                video.release()
+                    # tracking target face
+                    if len(face_list):
+                        # re-draw rect of target face area
+                        (x, y, w, h) = get_largest_image(face_list)
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), TARGET_RECT_COLOR, RECT_THICKNESS)
+
+                        # calculate the amount of camera-movement to center the target
+                        delta_x_angle, delta_y_angle = pixcel2angle(x + (w // 2), y + (h // 2))
+
+                        camera_x_angle += delta_x_angle * CORRECTION_FUCTOR
+                        camera_y_angle += delta_y_angle * CORRECTION_FUCTOR
+
+                        # limitation of upper and lower
+                        if camera_x_angle < MIN_X_CAMERA_ANGLE:
+                            camera_x_angle = MIN_X_CAMERA_ANGLE
+                        elif camera_x_angle > MAX_X_CAMERA_ANGLE:
+                            camera_x_angle = MAX_X_CAMERA_ANGLE
+
+                        if camera_y_angle < MIN_Y_CAMERA_ANGLE:
+                            camera_y_angle = MIN_Y_CAMERA_ANGLE
+                        elif camera_y_angle > MAX_Y_CAMERA_ANGLE:
+                            camera_y_angle = MAX_Y_CAMERA_ANGLE
+
+                        # change camera position
+                        camera_mount.position(camera_x_angle, camera_y_angle)
+
+                        print("delta", ("{:.2f}".format(delta_x_angle), "{:.2f}".format(delta_y_angle)))
+                        print("camera", ("{:.2f}".format(camera_x_angle), "{:.2f}".format(camera_y_angle)))
+                        print()
+
+                    # show image
+                    if SHOW_IMAGE:
+                        cv2.imshow(WINDOW_TITLE, frame)
+
+                    # write video
+                    if WRITE_VIDEO:
+                        video.write(frame)
+
+                    # key wait for escape
+                    if cv2.waitKey(KEY_WAIT_TIME) & 0xFF == ESC_KEY_NUM:
+                        break
+
+            except KeyboardInterrupt:
+                print('quit face tracking')
+            
+            finally:
+                if SHOW_IMAGE:
+                    cv2.destroyAllWindows()
+
+                if WRITE_VIDEO:
+                    video.release()
